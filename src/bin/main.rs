632bfb53c606bc7823e56174baf1c1574b1fe9c1
Ricardo Massaro <ricardo.massaro@gmail.com>
2016-08-27 13:54:11 -0300

    Add license to Cargo.toml

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,9 +12,14 @@ fn main() {
     //bleep.dump_env();
     //bleep.dump_funcs();
     
+    //let start = time::precise_time_ns();
+
     //println!("Calling script's function 'main':");
     match bleep.exec("main", &[Value::Number(42.0)]) {
         Ok(v) => println!("-> {}", v),
         Err(e) => println!("{}", e),
-    }    
+    }
+    
+    //let end = time::precise_time_ns();
+    //println!("time: {}ms", (end - start) / 1_000_000);
 }
